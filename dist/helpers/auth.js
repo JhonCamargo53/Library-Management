17839a01fb4faf0b21c35f04474cafc28884fc2f
Jhon Camargo <jcamargocelin@gmail.com>
2023-09-26 21:35:46 -0500

    add: Token Decode

--- dist/helpers/auth.js
+++ dist/helpers/auth.js
@@ -3,9 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateToken = void 0;
+exports.tokenDecode = exports.generateToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const generateToken = (user) => {
     return jsonwebtoken_1.default.sign({ user }, process.env.JWTKEY || 'NOT TOKEN USED', { expiresIn: '60m' });
 };
 exports.generateToken = generateToken;
+const tokenDecode = (token) => {
+    const validatedToken = jsonwebtoken_1.default.verify(token, process.env.JWTKEY || 'NOT TOKEN USED');
+    return JSON.parse(validatedToken.user);
+};
+exports.tokenDecode = tokenDecode;
--- src/helpers/Auth.ts
+++ src/helpers/Auth.ts
@@ -1,5 +1,12 @@
 import jwt from 'jsonwebtoken'
+import { IUser } from '../interface'
 
 export const generateToken = (user: string) => {
     return jwt.sign({user}, process.env.JWTKEY || 'NOT TOKEN USED', { expiresIn: '60m' } )
+}
+
+export const tokenDecode = (token: string) => {
+    const validatedToken = jwt.verify(token, process.env.JWTKEY || 'NOT TOKEN USED') as any
+    return  JSON.parse(validatedToken.user) as IUser;
+    
 }
